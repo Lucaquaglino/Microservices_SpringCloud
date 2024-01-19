94950a5b761a9eac9f59cfe142f0311661e5a13a
Luca Quaglino <quagli.luca@gmail.com>
2024-01-19 21:15:47 +0100

    commit

---
+++ currency-exchange/currency-exchange-services/src/main/java/com/lucaquaglino/microservices/currencyexchangeservices/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.lucaquaglino.microservices.currencyexchangeservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+	CurrencyExchange findByFromAndTo(String from, String to);
+
+}
