118fbdc7005a894c7bfe713b45d907f19b053835
Luca Quaglino <quagli.luca@gmail.com>
2024-01-20 05:01:20 +0100

    add configuration zipkin

--- currency-exchange/currency-exchange-services/src/main/java/com/lucaquaglino/microservices/currencyexchangeservices/CurrencyExchangeController.java
+++ currency-exchange/currency-exchange-services/src/main/java/com/lucaquaglino/microservices/currencyexchangeservices/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.lucaquaglino.microservices.currencyexchangeservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private CurrencyExchangeRepository repository;
 
@@ -17,7 +21,10 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-//		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		logger.info("retreiveExchangeValue called with {} to {}", from, to);
+
+		// CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,
+		// BigDecimal.valueOf(50));
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
 			throw new RuntimeException("unable to find data for " + from + "to" + to);
